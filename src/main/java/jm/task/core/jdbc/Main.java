b2c2931e301ac366822754e7e4f50dddb7509bd6
sharimardinov <sharimardinov@icloud.com>
2024-09-26 21:36:10 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,9 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
